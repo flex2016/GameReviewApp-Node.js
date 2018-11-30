bc153cdc7e28cc04b9bfacb9029386bc968ae20e
TARAS KURILO <reloded2004@hotmail.com>
2018-11-30 18:33:19 +0000

    added landing page

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+    
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The GameReview Server Has Started!");
+});
